126fc9526f00d0c2c7dbfb2b2dd106460aa776a1
Kudae Sithu <kudaesithu2@gmail.com>
2025-06-30 15:56:45 +0630

    finish-desktop-header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ const page = () => {
   return (
     <div>
       <p className="text-accent">
-        page <button className="primary-btn">Hello click me</button>{" "}
+        page <button className="btn-primary">Hello click me</button>{" "}
       </p>
+      <p className="text-secondary">Secondary</p>
     </div>
   );
 };
