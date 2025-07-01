6269368b849b2b4a14526b2caddf592c37a8d7d2
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-01 12:05:09 +0630

    Add layout, routing, and page components; integrate Navbar and Footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import "../globals.css";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
