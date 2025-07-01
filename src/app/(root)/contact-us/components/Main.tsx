a18158a5356ef32b3d0b80c9333d97967291f5cc
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-01 13:07:32 +0630

    Add Hero and Main components for Contact Us page; update styles and structure

---
+++ src/app/(root)/contact-us/components/Main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="space-y-5 text-center">
+        <h2 className="heading-title">We are Here to Help</h2>
+        <p className="heading-subtitle">
+          Whether you're curious about features, a free trail or even press-
+          we're ready to answer any and all questions
+        </p>
+      </div>
+      <div className="md:flex justify-center items-center gap-4">
+        {/* Accordion */}
+        {/* Contact Form */}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/app/(root)/contact-us/page.tsx
+++ src/app/(root)/contact-us/page.tsx
@@ -1,32 +1,12 @@
-import Image from "next/image";
 import React from "react";
+import Hero from "./components/Hero";
+import Main from "./components/Main";
 
 const page = () => {
   return (
-    <div className="bg-secondary">
-      <div className="h-[600px] sm:h-[400px] md:h-[400px] max-w-6xl mx-auto">
-        <div className="sm:grid grid-cols-2 p-10">
-          <div className="text-white space-y-7 flex flex-col justify-center items-start">
-            <h1 className="heading-title">Contact Us</h1>
-            <h2 className="heading-text">
-              Contact If you have any questions about our mobile app, services,
-              partnership enquiries, or anything else, drop us a note below. We
-              will get back to you as soon as possible.
-            </h2>
-            <button className="btn-primary px-4 py-2 rounded-lg cursor-pointer text-sm">
-              Call Us
-            </button>
-          </div>
-          <div className="flex justify-center items-center">
-            <Image
-              src="/images/female-health-gp.png"
-              width={300}
-              height={300}
-              alt="Contact Us Image"
-            />
-          </div>
-        </div>
-      </div>
+    <div className="space-y-10">
+      <Hero />
+      <Main />
     </div>
   );
 };
